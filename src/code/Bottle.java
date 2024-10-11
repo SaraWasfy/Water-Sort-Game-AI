e2e5698a10e34c46bf3ac8018e16d59054722ade
SaraWasfy <sara_a.wasfy@hotmail.com>
2024-10-11 05:23:17 +0300

    Resolved multiple changelist issues and updated Bottle, Node, Problem classes

--- src/code/Bottle.java
+++ src/code/Bottle.java
@@ -6,7 +6,8 @@ public class Bottle {
     private static int IDCounter = 0;
 
     private int ID;
-    private int capacity;
+    private static int capacity;
+
     private ArrayList<Character> layers;
 
     public Bottle(int capacity, ArrayList<Character> layers) {
@@ -14,4 +15,94 @@ public class Bottle {
         this.layers = layers;
         this.ID = IDCounter++;
     }
+
+    public void printBottle(){
+        for (char c: layers)
+            if (c!= layers.get(layers.size()-1))
+                System.out.print(c+",");
+            else
+                System.out.print(c);
+    }
+    public char getTopLayer() {
+
+        for (int i = 0; i < layers.size(); i++) {
+            char layer = layers.get(i);
+
+
+            if (layer != 'e') {
+                return layer;
+            }
+        }
+        return 'e';
+    }
+    public int getTopLayerIndex() {
+
+        for (int i = 0; i < layers.size(); i++) {
+            char layer = layers.get(i);
+
+
+            if (layer != 'e') {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public boolean isFull() {
+        return layers.size() == capacity;
+    }
+
+    public boolean isEmpty() {
+        ArrayList<Character> layers = this.getLayers();
+        for (char layer : layers) {
+            if (layer != 'e') {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int getTopLayerAmountSrc(char color) {
+        int topLayerCount = 0;
+        int i =0;
+
+
+        while(i<layers.size()) {
+            if (layers.get(i) == 'e') {
+                  i++;
+            }
+            else if(layers.get(i)==color){
+                topLayerCount++;
+                i++;
+
+            }
+            else
+                return topLayerCount;
+
+
+        }
+
+        return topLayerCount;
+    }
+
+    public int getEmptySpaceDest() {
+        return capacity - layers.size();
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public ArrayList<Character> getLayers() {
+        return layers;
+    }
+
+    public void setLayers(ArrayList<Character> layers) {
+        this.layers = layers;
+    }
+
 }
--- src/code/Node.java
+++ src/code/Node.java
@@ -1,16 +1,46 @@
 package code;
 
+import java.util.ArrayList;
+
 public class Node {
-    private Object state;
+
+    private ArrayList<Bottle>bottles;
+    private String action;
+    private int pathCost;
+    private int depth;
     private Node parent;
 
-    public Node(Object state, Node parent) {
-        this.state = state;
+    public Node(String action, int pathCost, Node parent, int depth, ArrayList<Bottle>bottles) {
+        this.action = action;
+        this.pathCost = pathCost;
         this.parent = parent;
+        this.depth = depth;
+        this.bottles=bottles;
     }
 
-    public Object getState() {
-        return state;
+    public void printNode(){
+        System.out.print("Level: "+depth+", Action: " + action + ", Path Cost: " + pathCost
+        + ", Bottles: ");
+        for (Bottle b: bottles){
+            b.printBottle();
+            System.out.print(";");
+        }
+    }
+
+    public ArrayList<Bottle> getBottles() {
+        return bottles;
+    }
+
+    public int getPathCost() {
+        return pathCost;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public int getDepth() {
+        return depth;
     }
 
     public Node getParent() {
